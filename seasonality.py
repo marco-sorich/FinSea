8aa231e91585ded750793fb728a3c1c53e9af211
Marco Sorich <marco@sorich.de>
2023-10-28 09:30:12 +0200

    Argument parser simplified and defaults more plausible

--- seasonality.py
+++ seasonality.py
@@ -7,7 +7,7 @@ import seasonality as ssn
 # select the symbol to analyze
 
 # symbol = '2B7K.DE'    # iShares MSCI World SRI UCITS ETF EUR (Acc)
-# symbol = 'EUNL.DE'    # iShares Core MSCI World UCITS ETF USD (Acc)
+symbol = 'EUNL.DE'    # iShares Core MSCI World UCITS ETF USD (Acc)
 # symbol = 'EURUSD=X'   # USD/EUR
 # symbol = 'GBPUSD=X'   # GBP/USD
 # symbol = 'AUDUSD=X'   # AUD/USD
@@ -18,7 +18,7 @@ import seasonality as ssn
 # symbol = 'AXP'        # American Express Company
 # symbol = 'BTC-USD'    # Bitcoin USD
 # symbol = 'ETH-USD'    # Ethereum USD
-symbol = '^GSPC'      # S&P 500
+# symbol = '^GSPC'      # S&P 500
 # symbol = 'AAPL'       # Apple
 
 
@@ -26,20 +26,26 @@ symbol = '^GSPC'      # S&P 500
 max_num_of_years = 5
 
 
+def view_type(view_str: str) -> ssn.Views:
+    if view_str == 'console':
+        return ssn.Views.CONSOLE
+    elif view_str == 'pdf':
+        return ssn.Views.PDF
+    else:
+        raise argparse.ArgumentTypeError(f'Invalid view: {view_str}')
+
+
 parser = argparse.ArgumentParser(description='Seasonality Analyzer')
-parser.add_argument('-s', '--symbol', type=str, default=symbol, help='Ticker symbol to analyze')
-parser.add_argument('-y', '--years', type=int, default=max_num_of_years, help='Maximum number of years to analyze backwards')
-parser.add_argument('-v', '--view', type=str, default='console', help='View to render the results (''console'' or ''pdf'')')
-parser.add_argument('-f', '--file', type=str, default='myPlots.pdf', help='File name to save the results (optional for ''console'' view))')
+parser.add_argument('-s', '--symbol', type=str, default=symbol, help='Ticker symbol to analyze (default: ' + symbol + ')')
+parser.add_argument('-y', '--years', type=int, default=max_num_of_years, help='Maximum number of years to analyze backwards (default: ' + str(max_num_of_years) + ')')
+parser.add_argument('-v', '--view', type=view_type, default=ssn.Views.CONSOLE, help='View to render the results (''console'' or ''pdf'')')
+parser.add_argument('-f', '--file', type=str, default='', help='File name to save the results (optional for ''console'' view)')
 
 
 analyzer = ssn.Analyzer(parser.parse_args().symbol, parser.parse_args().years)
 analyzer.calc()
 
-if parser.parse_args().view == 'console':
-    analyzer.render(ssn.Views.CONSOLE)
-elif parser.parse_args().view == 'pdf':
-    analyzer.render(ssn.Views.PDF, parser.parse_args().file)
-    os.system('open myplots.pdf')
-else:
-    raise ValueError('Unknown view type')
+analyzer.render(parser.parse_args().view, parser.parse_args().file)
+
+if parser.parse_args().file != '':
+    os.system(f'open {parser.parse_args().file}')
